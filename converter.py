e7e67426f24e3388bab67eaf0e373e698b71bfbd
August <24bryce5@gmail.com>
2023-11-15 17:30:55 -0500

    Added

---
+++ converter.py
@@ -0,0 +1,31 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def main():
+    while True:
+        print("Temperature Converter")
+        print("1. Celsius to Fahrenheit")
+        print("2. Fahrenheit to Celsius")
+        
+        choice = input("Enter your choice (1/2): ")
+        
+        if choice == '1':
+            celsius = float(input("Enter temperature in Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"{celsius}°C is {fahrenheit}°F")
+        elif choice == '2':
+            fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"{fahrenheit}°F is {celsius}°C")
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+        
+        again = input("Do you want to convert another temperature? (y/n): ")
+        if again.lower() != 'y':
+            break
+
+if __name__ == "__main__":
+    main()
